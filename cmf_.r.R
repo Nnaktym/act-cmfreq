ddcb7d7c9aefeba73a0248b0d273fa9b7f69158b
nanakato <nanaktym@gmail.com>
2024-09-21 19:39:21 +0900

    add: investigation script

--- cmf_.r.R
+++ cmf_.r.R
@@ -1,62 +1,39 @@
-# original
 
 library(cmfrec)
 library(ggplot2)
 
-# library(CASdatasets)
-# cas_dataset_path = "/Users/nanakato/projects/CASdatasets/data"
-#
-# load(paste(cas_dataset_path, "brvehins1a.rda", sep="/"))
-# load(paste(cas_dataset_path, "brvehins1b.rda", sep="/"))
-# load(paste(cas_dataset_path, "brvehins1c.rda", sep="/"))
-# load(paste(cas_dataset_path, "brvehins1d.rda", sep="/"))
-# load(paste(cas_dataset_path, "brvehins1e.rda", sep="/"))
-#
-# brvehins = rbind(brvehins1a, brvehins1b, brvehins1c, brvehins1d, brvehins1e)
-# write.csv(brvehins, file="brvehins.csv")
-brvehins = read.csv("data/brvehins.csv")
+# library(CASdatasets) ---> 環境の問題？かインストールに失敗したため、ローカルからデータを読み込んだ
+# data(brvehins1a)
+# data(brvehins1b)
+# data(brvehins1c)
+# data(brvehins1d)
+# data(brvehins1e)
+## ! cd /Users/nanakato/projects
+## ! git clone https://github.com/dutangc/CASdatasets.git
+cas_dataset_path = "/Users/nanakato/projects/CASdatasets/data"
+load(paste(cas_dataset_path, "brvehins1a.rda", sep="/"))
+load(paste(cas_dataset_path, "brvehins1b.rda", sep="/"))
+load(paste(cas_dataset_path, "brvehins1c.rda", sep="/"))
+load(paste(cas_dataset_path, "brvehins1d.rda", sep="/"))
+load(paste(cas_dataset_path, "brvehins1e.rda", sep="/"))
+brvehins = rbind(brvehins1a, brvehins1b, brvehins1c, brvehins1d, brvehins1e)
 
-# > str(brvehins)
-# 'data.frame':	1965355 obs. of  24 variables:
-#  $ X                  : int  565192 1549299 803785 1735441 1848349 89535 1037912 1753914 1083762 897406 ...
-#  $ Gender             : chr  "Female" "Female" "Female" "Male" ...
-#  $ DrivAge            : chr  ">55" "36-45" "18-25" ">55" ...
-#  $ VehYear            : int  1997 2010 2008 2004 2009 1998 1999 2006 2010 2002 ...
-#  $ VehModel           : chr  "Gm - Chevrolet - Kadett Gl 2.0 Mpfi / Efi" "Gm - Chevrolet - Montana 1.4 8v Conquest Econoflex  2p" "Vw - Volkswagen - Fox City 1.0mi/ 1.0mi Total Flex 8v 3p" "Harley-davidson - Fat Boy" ...
-#  $ VehGroup           : chr  "Gm Chevrolet Kadett" "Gm Chevrolet Montana" "Vw Volkswagen Fox 1.0" "Harley-davidson Motos - Todas" ...
-#  $ Area               : chr  "Interior" "Maranhao" "Mato Grosso do Sul" "Met. Porto Alegre e Caxias do Sul" ...
-#  $ State              : chr  "Rio de Janeiro" "Maranhao" "Mato Grosso do Sul" "Rio Grande do Sul" ...
-#  $ StateAb            : chr  "RJ" "MA" "MS" "RS" ...
-#  $ ExposTotal         : num  1.01 3 1.01 1.45 4.55 1 1.01 1.51 0.25 0.93 ...
-#  $ ExposFireRob       : int  0 0 0 0 0 0 0 0 0 0 ...
-#  $ PremTotal          : num  743 5026 916 1602 53031 ...
-#  $ PremFireRob        : int  0 0 0 0 0 0 0 0 0 0 ...
-#  $ SumInsAvg          : num  10853 31010 24977 35933 301890 ...
-#  $ ClaimNbRob         : int  0 0 0 0 0 0 0 0 0 0 ...
-#  $ ClaimNbPartColl    : int  0 0 0 0 1 1 0 0 0 0 ...
-#  $ ClaimNbTotColl     : int  0 0 0 0 0 0 0 0 0 0 ...
-#  $ ClaimNbFire        : int  0 0 0 0 0 0 0 0 0 0 ...
-#  $ ClaimNbOther       : int  0 0 0 0 0 2 0 0 0 0 ...
-#  $ ClaimAmountRob     : num  0 0 0 0 0 0 0 0 0 0 ...
-#  $ ClaimAmountPartColl: int  0 0 0 0 648 2773 0 0 0 0 ...
-#  $ ClaimAmountTotColl : num  0 0 0 0 0 0 0 0 0 0 ...
-#  $ ClaimAmountFire    : int  0 0 0 0 0 0 0 0 0 0 ...
-#  $ ClaimAmountOther   : int  0 0 0 0 0 120 0 0 0 0 ...
+cas_dataset_path = "/Users/nanakato/projects/CASdatasets/data"
+load(paste(cas_dataset_path, "brvehins1a.rda", sep="/"))
+load(paste(cas_dataset_path, "brvehins1b.rda", sep="/"))
+load(paste(cas_dataset_path, "brvehins1c.rda", sep="/"))
+load(paste(cas_dataset_path, "brvehins1d.rda", sep="/"))
+load(paste(cas_dataset_path, "brvehins1e.rda", sep="/"))
+brvehins = rbind(brvehins1a, brvehins1b, brvehins1c, brvehins1d, brvehins1e)
+
+
 
 premium_total = tapply(brvehins$PremTotal, list(brvehins$VehModel,brvehins$Area), sum) # 型・地域毎の保険料の集計
 claim_total = tapply(rowSums(brvehins[,19:23]), list(brvehins$VehModel,brvehins$Area), sum) # 型・地域毎のクレーム総額の集計
-
-
-
 # > dim(premium_total)
 # [1] 4259   40
 # > dim(claim_total)
 # [1] 4259   40
-
-# memo:
-# there are categories that has missing values
-
-# 料率区分が細かいとき、欠測が出たりふつうにあるし実績が安定しない。交互作用なんてみてられない。
 
 # 保険料計10000以上、クレーム総額計5000以上の型だけ残す
 premium_total_10000 = premium_total[rowSums(premium_total,na.rm=T)>=10000 & rowSums(claim_total,na.rm=T)>=5000,]
@@ -67,23 +44,6 @@ claim_total_10000[is.na(claim_total_10000)] = 0 # added
 
 loss_ratio = claim_total_10000 / premium_total_10000 # 型・地域毎の損害率
 loss_ratio[premium_total_10000==0] = NA # added
-
-# write.csv(loss_ratio, file="data/loss_ratio.csv")
-# > dim(lr)
-# [1] 2517   40
-
-
-# premium = ifelse(is.na(premium_total_10000),0, premium_total_10000) # weight
-# > wt = ifelse(is.na(pt10000),0,pt10000) # weight
-# > dim(wt)
-# [1] 2517   40
-
-# cmf = CMF_implicit(
-#   X=loss_ratio,
-#   # k=1,
-#   # weight=premium,
-#   # lambda=1000000,
-# )
 
 cmf = CMF(
   X=loss_ratio,
@@ -101,9 +61,17 @@ mu = cmf$matrices$glob_mean
 # 型・地域毎の損害率の予測値
 pred_loss_ratio = mu + type_bias%o%rep(1, ncol(loss_ratio)) + rep(1,nrow(loss_ratio))%o%region_bias + t(A)%*%B
 
-# 型・地域毎の損害率の予測値のヒートマップ
-image(pred_loss_ratio, xlab="type", ylab="region", breaks=0:12/10)
-image(loss_ratio, xlab="type",ylab="region",breaks=0:12/10) # 型・地域毎の損害率のヒートマップ
+# 型・地域毎の損害率のヒートマップ比較
+par(mfrow=c(1,2))
+image(pred_loss_ratio, xlab="type", ylab="region", breaks=0:12/10) # 予測値
+image(loss_ratio, xlab="type",ylab="region",breaks=0:12/10) # 実績値
+
+# 型・地域毎の損害率の散布図比較
+compare = data.frame(
+  pred = as.vector(pred_loss_ratio),
+  actual = as.vector(loss_ratio)
+)
+ggplot(compare,aes(x=pred,y=actual)) + geom_point() # 予測値と実績値の散布図
 
 areas = data.frame(
   area = colnames(loss_ratio),
